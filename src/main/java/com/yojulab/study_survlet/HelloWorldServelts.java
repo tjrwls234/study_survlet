45e97b5ae0c6752f50dfe76df562322e1bc54de7
tjrwls234 <tjrwls234@gmail.com>
2022-12-19 11:09:27 +0900

    add SimpleWithDB

--- src/main/java/com/yojulab/study_survlet/HelloWorldServelts.java
+++ src/main/java/com/yojulab/study_survlet/HelloWorldServelts.java
@@ -11,6 +11,10 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/helloWorldServlets")
 public class HelloWorldServelts extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String firstName = request.getParameter("firstName");
+        String secondName = request.getParameter("secondName");
+        String hiddenParam = request.getParameter("hiddenParam");
+
         PrintWriter printWriter = response.getWriter();
 
         String message = "HelloWorldServlets with Message!";
@@ -19,9 +23,16 @@ public class HelloWorldServelts extends HttpServlet {
         printWriter.println("<title>" + message + "</title>");
         printWriter.println("</head>");
         printWriter.println("<body>");
-        printWriter.println("<div>" + message + "</div>");
+        printWriter.println("<div> firstName : " + firstName + "</div>");
+        printWriter.println("<div> secondName : " + secondName + "</div>");
+        printWriter.println("<form action='/helloWorldServlets' method='get'>");
+        printWriter.println("<input type='text' name='firstName' id='' value='" + firstName + "' />");
+        printWriter.println("<input type='text' name='secondName' id='' value='" + secondName + "' />");
+        printWriter.println("<button>recall helloWorldServlets</button>");
+        printWriter.println("</form>");
         printWriter.println("</body>");
         printWriter.println("</html>");
+
         printWriter.close();
     }
 }
